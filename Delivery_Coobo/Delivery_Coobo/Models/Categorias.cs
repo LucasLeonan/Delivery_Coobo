71c08b40aa1fc67ccb5956c769c1cdad09d83793
Lucas Moreno <lucas.moreno@coobo.com.br>
2015-06-01 14:43:39 -0300

    Atualização de classes
    Atualização de classes

--- Delivery_Coobo/Delivery_Coobo/Models/Categorias.cs
+++ Delivery_Coobo/Delivery_Coobo/Models/Categorias.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace Delivery_Coobo.Models
 {
     public class Categorias
     {
+        public Categorias()
+        {
+            Projeto = new List<Projetos>();
+        }
+
+        public int CategoriasID { get; set; }
+        [StringLength(120)]
+        public string Nome_Categoria { get; set; }
+        public string Descricao { get; set; }
+
+
+        public ICollection<Projetos> Projeto { get; set; }
     }
 }
--- Delivery_Coobo/Delivery_Coobo/Models/Empresas.cs
+++ Delivery_Coobo/Delivery_Coobo/Models/Empresas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,26 @@ namespace Delivery_Coobo.Models
 {
     public class Empresas
     {
+         public Empresas()
+        {
+            Project = new List<Projetos>();
+        }
+
+        public int Cliente_FornID { get; set; }
+        [StringLength(80)]
+        public string Nome_Empresa { get; set; }
+        [StringLength(80)]
+        public string Nome_Contato { get; set; }
+        [StringLength(50)]
+        public string Email {get;set;}
+        public string Telefone { get; set;}
+       [Range(0, int.MaxValue, ErrorMessage = "Inserir apenas múmeros")]
+        public string CNPJ { get; set; }
+        [StringLength(200)]
+        public string Descricao { get; set; }
+
+        public virtual ICollection<Projetos> Project { get; set;}
+        
+    }       
     }
 }
